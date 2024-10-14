23a4a7f1c9031ef8f868388f64d3867ba6ec8fea
unknown <ashuisalive@gmail.com>
2024-10-15 01:21:35 +0530

    stock buy and sell

--- Day-1 Arrays/StockBuyAndSell.js
+++ Day-1 Arrays/StockBuyAndSell.js
@@ -1,1 +1,16 @@
-
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let n = prices.length;
+  let maxProfit = 0;
+  let lsf = Number.MAX_SAFE_INTEGER;
+  let pst = 0;
+  for (let i = 0; i < n; i++) {
+    if (lsf > prices[i]) lsf = prices[i];
+    pst = prices[i] - lsf;
+    if (pst > maxProfit) maxProfit = pst;
+  }
+  return maxProfit;
+};
